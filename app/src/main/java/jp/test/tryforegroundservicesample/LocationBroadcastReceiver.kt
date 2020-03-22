ed97c39736c2146cbd49fbc740566a71a00e98b4
gnk263 <gnk263@users.noreply.github.com>
2020-03-22 10:01:26 +0900

    call stopService()

--- app/src/main/java/jp/test/tryforegroundservicesample/LocationBroadcastReceiver.kt
+++ app/src/main/java/jp/test/tryforegroundservicesample/LocationBroadcastReceiver.kt
@@ -6,5 +6,7 @@ import android.content.Intent
 
 class LocationBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
+        val targetIntent = Intent(context, LocationService::class.java)
+        context.stopService(targetIntent)
     }
 }
