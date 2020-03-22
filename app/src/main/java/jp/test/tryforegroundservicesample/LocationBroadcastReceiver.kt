77e3ee90857b535ecf7d7d6cc5b41d71c78ec6c7
gnk263 <gnk263@users.noreply.github.com>
2020-03-22 09:29:49 +0900

    add LocationBroadcastReceiver

---
+++ app/src/main/java/jp/test/tryforegroundservicesample/LocationBroadcastReceiver.kt
@@ -0,0 +1,10 @@
+package jp.test.tryforegroundservicesample
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class LocationBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+    }
+}
